69c82611efe22fc42a0361bea2ed078696eae22f
ifiz <ifiz@pm.me>
2025-04-13 06:48:56 -0400

    add-static-checks

--- repository/main.tf
+++ repository/main.tf
@@ -1,6 +1,7 @@
 resource "github_repository" "repository" {
   name                   = var.name
   description            = var.description
+  #checkov:skip=CKV_GIT_1: allow public repos
   visibility             = var.visibility
   is_template            = var.is_template
   delete_branch_on_merge = var.delete_branch_on_merge
@@ -47,6 +48,7 @@ resource "github_branch_protection" "branch_protection" {
   allows_force_pushes             = false
   allows_deletions                = false
   require_conversation_resolution = true
+  require_signed_commits          = true
 
   required_status_checks {
     strict = true
@@ -60,6 +62,7 @@ resource "github_branch_protection" "branch_protection" {
     dismiss_stale_reviews           = true
     require_code_owner_reviews      = true
     require_last_push_approval      = true
+    #checkov:skip=CKV_GIT_5: only one approval should suffice
     required_approving_review_count = 1
     pull_request_bypassers          = var.bypass_teams
     restrict_dismissals             = true
